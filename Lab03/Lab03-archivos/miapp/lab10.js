bc6af8f6c4ea40987974f853b44b6ec9330be477
Your Name <you@example.com>
2019-03-29 23:00:34 -0500

    Lab03 resuelto

---
+++ Lab03/Lab03-archivos/miapp/lab10.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  producto = require('./models/prod'),
+  usuarios = require('./models/user');
+//var path = require('path');
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.set('view engine','jade');
+
+app.get('/', function(req, res) {
+  res.send('Hola mundo');
+});
+
+/*app.get('/login', function(req, res) {
+  res.render('login');
+});*/
+
+app.get('/table', function(req, res) {
+  res.render('table');
+});
+
+app.get('/producto', producto.show);
+app.post('/producto', producto.create);
+app.post('/producto/update', producto.update);
+app.post('/producto/delete', producto.delete);
+
+app.get('/login', usuarios.show);
+app.post('/login', usuarios.logear);
+
+app.listen(9090, function() {
+  console.log('Iniciando!');
+});
