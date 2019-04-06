70bf757dc40ea3a0dcee2c2a4914b4069e28d7d8
Your Name <you@example.com>
2019-04-06 18:07:51 -0500

    Lab05 resuelto

--- Lab05/proyectos/lab05/models/user.js
+++ Lab05/proyectos/lab05/models/user.js
@@ -25,5 +25,31 @@ module.exports = {
     };
     var nuevo = new user_model(item).save();
     callback(item);
+  },
+  show: function(callback) {
+    user_model.find({}, function(err, items) {
+      if(!err) {
+        callback(JSON.stringify(items));
+      }else {
+        return console.log(err);
+      }
+    });
+  },
+  update: function(data, callback) {
+    user_model.findOne({_id: data._id}, function(err, item) {
+      item.first_name = data.first_name;
+      item.last_name = data.last_name;
+      item.timezone = data.timezone;
+      item.locale = data.locale;
+      item.profile_pic = data.profile_pic;
+      item.save();
+      callback(item);
+    })
+  },
+  delete: function(_id, callback) {
+    user_model.findOne({_id: _id}, function(err, post) {
+      post.remove();
+      callback(_id);
+    });
   }
 };
--- Lab05/proyectos/lab05/server.js
+++ Lab05/proyectos/lab05/server.js
@@ -16,32 +16,43 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
   console.log('Usuario conectado!');
   //Funcion para conseguir datos desde el jade
+
+  user.show(function(data) {
+    //console.log(data);
+    socket.emit('listar', data);
+  });
+
+  socket.on('actualizar', function(data) {
+    user.update(data, function(rpta) {
+      io.emit('nuevo', rpta);
+    });
+  });
+
+  socket.on('eliminar', function(data) {
+    user.delete(data, function(rpta) {
+      io.emit('borrado', rpta);
+    });
+  });
+
   socket.on('crear', function(data) {
     user.create(data, function(rpta) {
       io.emit('nuevo',rpta);
-    })
+    });
   })
   //Funcion para agregar datos a la bd
   socket.on('nuevo', function(data) {
     fill(data);
   });
-  var fill = function(data) {
-    var $row = $('<tr id="'+data._id+'">');
-    $row.append('<td>'+data._id+'</td>');
-    $row.append('<td>'+data.first_name+'</td>');
-    $row.append('<td>'+data.last_name+'</td>');
-    $row.append('<td>'+data.timezone+'</td>');
-    $row.append('<td>'+data.locale+'</td>');
-    $row.append('<td>'+data.profile_pic+'</td>');
-    $row.append('<td><button class="btn btn-success btn-sm" name="btnAct">Actualizar</button></td>');
-    $row.append('<td><button class="btn btn-danger btn-sm" name="btnEli">Eliminar</button></td>');
-    $row.data('data', data);
-    $('table tbody').append($row);
-  }
+
+
   //Funcion para avisar cuando alguien se desconecta
   socket.on('disconnect', function() {
     console.log('Usuario desconectado!');
   });
+
+  //Nos muestra la lsita de los datos
+
+
 });
 
 
